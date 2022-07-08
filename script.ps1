3a131a3a31063c8b48c4c16ff21275a0ba52231e
Ahmed Kamran <akamran@colgate.edu>
2022-07-08 13:44:57 -0400

    script working dir specified

--- script.ps1
+++ script.ps1
@@ -1,10 +1,12 @@
 $exelus = Get-Process Exelus.Win11DesktopSwitchAnimatior -ErrorAction SilentlyContinue
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
 if ($exelus) {
   if (!$exelus.HasExited) {
     $exelus | Stop-Process -Force
   }
 }
 else{
-    Start-Process -FilePath "Exelus.Win11DesktopSwitchAnimatior\Exelus.Win11DesktopSwitchAnimatior\bin\Debug\net6.0-windows\publish\Exelus.Win11DesktopSwitchAnimatior.exe"
+    Start-Process -FilePath "Exelus.Win11DesktopSwitchAnimatior\Exelus.Win11DesktopSwitchAnimatior\bin\Debug\net6.0-windows\publish\Exelus.Win11DesktopSwitchAnimatior.exe" -WorkingDirectory $dir
 }
 Remove-Variable exelus
