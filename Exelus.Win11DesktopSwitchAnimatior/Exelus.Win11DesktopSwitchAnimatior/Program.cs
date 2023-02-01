55f7f1db3d95e64641ce78253b8193f051805c1e
sanket kheni <68942390+sanketkheni01@users.noreply.github.com>
2023-02-01 17:01:02 +0530

    Add quick peek

--- Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
+++ Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using Exelus.Win11DesktopSwitchAnimatior;
 using Microsoft.Win32.TaskScheduler;
 using System.Security.Principal;
+using System.Diagnostics;
 
 TaskService ts = new TaskService();
 TaskDefinition td = ts.NewTask();
@@ -12,11 +13,16 @@ string appPath = AppContext.BaseDirectory;
 string exeName = Assembly.GetExecutingAssembly().GetName().Name;
 string exePath = appPath + exeName + ".exe";
 string taskName = "Windows 11 Virtual desktop animation";
+
+bool isSwitching = false;
+
+
+
 if (ts.FindTask(taskName) == null)
 {
     td.Actions.Add(new ExecAction("\"" + exePath + "\"", "", null));
 
-    td.Principal.RunLevel = TaskRunLevel.Highest; 
+    td.Principal.RunLevel = TaskRunLevel.Highest;
     // get default userid
     WindowsIdentity identity = WindowsIdentity.GetCurrent();
 
@@ -32,6 +38,7 @@ TouchInjector.InitializeTouchInjection(4, TouchFeedback.NONE);
 
 bool FourFingerSwipe(int distance, int time)
 {
+    isSwitching = true;
     var touches = new PointerTouchInfo[4];
     for (int i = 0; i < 4; i++)
     {
@@ -62,45 +69,104 @@ bool FourFingerSwipe(int distance, int time)
         touches[i].PointerInfo.PointerFlags = PointerFlags.UPDATE | PointerFlags.INRANGE | PointerFlags.INCONTACT;
     }
     TouchInjector.InjectTouchInput(4, touches);
-    Thread.Sleep(time);
 
     for (int i = 0; i < 4; i++)
     {
         touches[i].PointerInfo.PtPixelLocation.X += distance;
     }
     TouchInjector.InjectTouchInput(4, touches);
+   
     for (int i = 0; i < 4; i++)
     {
         touches[i].PointerInfo.PtPixelLocation.X += distance;
         touches[i].PointerInfo.PointerFlags = PointerFlags.UP;
     }
     TouchInjector.InjectTouchInput(4, touches);
-
+    isSwitching = false;
     return true;
 }
 
-
 while (true)
 {
-    Thread.Sleep(5);
     var ctrlState = GetAsyncKeyState(17);
     var winState = GetAsyncKeyState(91);
     var upArrowState = GetAsyncKeyState(38);
     var downArrowState = GetAsyncKeyState(40);
+    var altKeyState = GetAsyncKeyState(18);
+    Thread.Sleep(5);
     /*
         * * Get Key State https://www.amibroker.com/guide/afl/getasynckeystate.html
     */
-    if (ctrlState != 0 && winState != 0 && upArrowState != 0)
-    {
-        FourFingerSwipe(50, 0);
-        Thread.Sleep(300);
-    }
-    if (ctrlState != 0 && winState != 0 && downArrowState != 0)
-    {
-        FourFingerSwipe(-50, 0);
-        Thread.Sleep(300);
-    }
 
+    Trace.WriteLine("This is a log message." + isSwitching);
+
+    if (ctrlState != 0 && winState != 0 && upArrowState != 0 && !isSwitching)
+    {
+        FourFingerSwipe(100, 200);
+        Thread.Sleep(200);
+    }
+    if (ctrlState != 0 && winState != 0 && downArrowState != 0 && !isSwitching)
+    {
+        FourFingerSwipe(-100, 200);
+        Thread.Sleep(200);
+    }
+    if (altKeyState != 0 && winState != 0)
+    {
+        Hold(-200, 500);
+    }
 }
 
+void Hold(int distance, int time)
+{
+    var touches = new PointerTouchInfo[4];
+    for (int i = 0; i < 4; i++)
+    {
+        var touch = new PointerTouchInfo()
+        {
+            PointerInfo = new PointerInfo()
+            {
+                PtPixelLocation = new TouchPoint()
+                {
+                    X = 200,
+                    Y = (i * 100) + 100,
+                },
+                pointerType = PointerInputType.TOUCH,
+                PointerFlags = PointerFlags.DOWN | PointerFlags.INRANGE | PointerFlags.INCONTACT,
+                PointerId = (uint)i,
+            },
+            Orientation = 90,
+            Pressure = 100,
+            TouchMasks = TouchMask.CONTACTAREA | TouchMask.ORIENTATION | TouchMask.PRESSURE
+        };
+        touches[i] = (touch);
+    }
 
+    TouchInjector.InjectTouchInput(4, touches);
+    for (int i = 0; i < 4; i++)
+    {
+        touches[i].PointerInfo.PtPixelLocation.X += distance;
+        touches[i].PointerInfo.PointerFlags = PointerFlags.UPDATE | PointerFlags.INRANGE | PointerFlags.INCONTACT;
+    }
+    while (true)
+    {
+        Thread.Sleep(5);
+        var altKeyState = GetAsyncKeyState(18);
+        var ctrlState = GetAsyncKeyState(17);
+        var winState = GetAsyncKeyState(91);
+        var upArrowState = GetAsyncKeyState(38);
+
+        if (!(altKeyState != 0 && winState != 0))
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                touches[i].PointerInfo.PointerFlags = PointerFlags.UP;
+            }
+            TouchInjector.InjectTouchInput(4, touches);
+            break;
+        }
+        else
+        {
+            TouchInjector.InjectTouchInput(4, touches);
+        }
+    }
+}
