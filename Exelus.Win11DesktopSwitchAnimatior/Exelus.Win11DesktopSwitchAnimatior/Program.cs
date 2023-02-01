efd495fab132db10e8b28da736837ebf73e21ed7
sanket kheni <68942390+sanketkheni01@users.noreply.github.com>
2023-02-01 18:29:13 +0530

    refactor: clean and optimize code.

--- Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
+++ Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
@@ -1,85 +1,20 @@
 ﻿using System.Runtime.InteropServices;
-using Microsoft.Win32;
-using System.Reflection;
 using Exelus.Win11DesktopSwitchAnimatior;
-using Microsoft.Win32.TaskScheduler;
-using System.Security.Principal;
-using System.Diagnostics;
-
-TaskService ts = new TaskService();
-TaskDefinition td = ts.NewTask();
-
-string appPath = AppContext.BaseDirectory;
-string exeName = Assembly.GetExecutingAssembly().GetName().Name;
-string exePath = appPath + exeName + ".exe";
-string taskName = "Windows 11 Virtual desktop animation";
-
-bool isSwitching = false;
-
-Assembly assembly = Assembly.GetExecutingAssembly();
-string? version = assembly.GetName().Version?.ToString();
-
-Trace.WriteLine("This is a log message." + version);
-
-void createTask()
-{
-    td.Actions.Add(new ExecAction("\"" + exePath + "\"", "", null));
-
-    td.Principal.RunLevel = TaskRunLevel.Highest;
-    // get default userid
-    WindowsIdentity identity = WindowsIdentity.GetCurrent();
-    td.RegistrationInfo.Description = version;
-    // Get the user name
-    string userName = identity.Name;
-    td.Triggers.Add(new LogonTrigger());
-    ts.RootFolder.RegisterTaskDefinition(taskName, td, TaskCreation.CreateOrUpdate, userName, null, TaskLogonType.InteractiveToken);
-}
-
-Microsoft.Win32.TaskScheduler.Task task = ts.FindTask(taskName);
-string des = task.Definition.RegistrationInfo.Description;
-if (task == null)
-{
-    createTask();
-
-}
-else
-{
-    if (des != version)
-    {
-        ts.RootFolder.DeleteTask(taskName);
-        createTask();
-    }
-}
+using Windows_11_Virtual_desktop_animation;
 
 [DllImport("user32")]
 static extern int GetAsyncKeyState(int i);
 TouchInjector.InitializeTouchInjection(4, TouchFeedback.NONE);
 
+bool isSwitching = false;
+
+// handles the task scheduler
+TaskSchedulerM ts = new();
+
 bool FourFingerSwipe(int distance, int time)
 {
     isSwitching = true;
-    var touches = new PointerTouchInfo[4];
-    for (int i = 0; i < 4; i++)
-    {
-        var touch = new PointerTouchInfo()
-        {
-            PointerInfo = new PointerInfo()
-            {
-                PtPixelLocation = new TouchPoint()
-                {
-                    X = 200,
-                    Y = (i * 100) + 100,
-                },
-                pointerType = PointerInputType.TOUCH,
-                PointerFlags = PointerFlags.DOWN | PointerFlags.INRANGE | PointerFlags.INCONTACT,
-                PointerId = (uint)i,
-            },
-            Orientation = 90,
-            Pressure = 32000,
-            TouchMasks = TouchMask.CONTACTAREA | TouchMask.ORIENTATION | TouchMask.PRESSURE
-        };
-        touches[i] = (touch);
-    }
+    var touches = createTouchPoints();
 
     TouchInjector.InjectTouchInput(4, touches);
     for (int i = 0; i < 4; i++)
@@ -117,8 +52,6 @@ while (true)
         * * Get Key State https://www.amibroker.com/guide/afl/getasynckeystate.html
     */
 
-    Trace.WriteLine("This is a log message." + isSwitching);
-
     if (ctrlState != 0 && winState != 0 && upArrowState != 0 && !isSwitching)
     {
         FourFingerSwipe(100, 200);
@@ -137,29 +70,8 @@ while (true)
 
 void Hold(int distance, int time)
 {
-    var touches = new PointerTouchInfo[4];
-    for (int i = 0; i < 4; i++)
-    {
-        var touch = new PointerTouchInfo()
-        {
-            PointerInfo = new PointerInfo()
-            {
-                PtPixelLocation = new TouchPoint()
-                {
-                    X = 200,
-                    Y = (i * 100) + 100,
-                },
-                pointerType = PointerInputType.TOUCH,
-                PointerFlags = PointerFlags.DOWN | PointerFlags.INRANGE | PointerFlags.INCONTACT,
-                PointerId = (uint)i,
-            },
-            Orientation = 90,
-            Pressure = 100,
-            TouchMasks = TouchMask.CONTACTAREA | TouchMask.ORIENTATION | TouchMask.PRESSURE
-        };
-        touches[i] = (touch);
-    }
-
+    var touches = createTouchPoints();
+        
     TouchInjector.InjectTouchInput(4, touches);
     for (int i = 0; i < 4; i++)
     {
@@ -188,4 +100,31 @@ void Hold(int distance, int time)
             TouchInjector.InjectTouchInput(4, touches);
         }
     }
+}
+
+PointerTouchInfo[] createTouchPoints()
+{
+    var touches = new PointerTouchInfo[4];
+    for (int i = 0; i < 4; i++)
+    {
+        var touch = new PointerTouchInfo()
+        {
+            PointerInfo = new PointerInfo()
+            {
+                PtPixelLocation = new TouchPoint()
+                {
+                    X = 200,
+                    Y = (i * 100) + 100,
+                },
+                pointerType = PointerInputType.TOUCH,
+                PointerFlags = PointerFlags.DOWN | PointerFlags.INRANGE | PointerFlags.INCONTACT,
+                PointerId = (uint)i,
+            },
+            Orientation = 90,
+            Pressure = 100,
+            TouchMasks = TouchMask.CONTACTAREA | TouchMask.ORIENTATION | TouchMask.PRESSURE
+        };
+        touches[i] = (touch);
+    }
+    return touches;
 }
---
+++ Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/TaskSchedulerM.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32.TaskScheduler;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace Windows_11_Virtual_desktop_animation
+{
+    internal class TaskSchedulerM
+    {
+        TaskDefinition td;
+        string exePath;
+        string version;
+        string taskName;
+        TaskService ts;
+
+        public TaskSchedulerM()
+        {
+            ts = new TaskService();
+            td = ts.NewTask();
+
+            string appPath = AppContext.BaseDirectory;
+            string exeName = Assembly.GetExecutingAssembly().GetName().Name;
+            exePath = appPath + exeName + ".exe";
+            taskName = "Windows 11 Virtual desktop animation";
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            version = assembly.GetName().Version?.ToString();
+
+            Microsoft.Win32.TaskScheduler.Task task = ts.FindTask(taskName);
+            string des = task.Definition.RegistrationInfo.Description;
+            if (task == null)
+            {
+                createTask();
+            }
+            else
+            {
+                if (des != version)
+                {
+                    ts.RootFolder.DeleteTask(taskName);
+                    createTask();
+                }
+            }
+        }
+
+
+        void createTask()
+        {
+            td.Actions.Add(new ExecAction("\"" + exePath + "\"", "", null));
+
+            td.Principal.RunLevel = TaskRunLevel.Highest;
+            // get default userid
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            td.RegistrationInfo.Description = version;
+            // Get the user name
+            string userName = identity.Name;
+            td.Triggers.Add(new LogonTrigger());
+            ts.RootFolder.RegisterTaskDefinition(taskName, td, TaskCreation.CreateOrUpdate, userName, null, TaskLogonType.InteractiveToken);
+        }
+
+    }
+}
