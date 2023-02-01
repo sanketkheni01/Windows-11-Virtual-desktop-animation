e54c545b7861c493deaa2085395fdd144b7a58c6
sanket kheni <68942390+sanketkheni01@users.noreply.github.com>
2023-02-01 17:18:40 +0530

    v1.0.3

--- Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
+++ Exelus.Win11DesktopSwitchAnimatior/Exelus.Win11DesktopSwitchAnimatior/Program.cs
@@ -16,20 +16,39 @@ string taskName = "Windows 11 Virtual desktop animation";
 
 bool isSwitching = false;
 
+Assembly assembly = Assembly.GetExecutingAssembly();
+string version = assembly.GetName().Version.ToString();
 
+Trace.WriteLine("This is a log message." + version);
 
-if (ts.FindTask(taskName) == null)
+void createTask()
 {
     td.Actions.Add(new ExecAction("\"" + exePath + "\"", "", null));
 
     td.Principal.RunLevel = TaskRunLevel.Highest;
     // get default userid
     WindowsIdentity identity = WindowsIdentity.GetCurrent();
-
+    td.RegistrationInfo.Description = version;
     // Get the user name
     string userName = identity.Name;
     td.Triggers.Add(new LogonTrigger());
     ts.RootFolder.RegisterTaskDefinition(taskName, td, TaskCreation.CreateOrUpdate, userName, null, TaskLogonType.InteractiveToken);
+}
+
+Microsoft.Win32.TaskScheduler.Task task = ts.FindTask(taskName);
+string des = task.Definition.RegistrationInfo.Description;
+if (task == null)
+{
+    createTask();
+
+}
+else
+{
+    if (des != version)
+    {
+        ts.RootFolder.DeleteTask(taskName);
+        createTask();
+    }
 }
 
 [DllImport("user32")]
@@ -75,7 +94,7 @@ bool FourFingerSwipe(int distance, int time)
         touches[i].PointerInfo.PtPixelLocation.X += distance;
     }
     TouchInjector.InjectTouchInput(4, touches);
-   
+
     for (int i = 0; i < 4; i++)
     {
         touches[i].PointerInfo.PtPixelLocation.X += distance;
